59102744c2f7f259cc8f79905d330fefafd07ff0
Consecratio <jcasique@gmail.com>
2021-08-24 11:22:58 -0700

    added find nth prime

---
+++ find_nth_prime.js
@@ -0,0 +1,10 @@
+const findNthPrime = () => {
+    return 0
+}
+
+console.log(findNthPrime(5))
+
+// let newArr = ["A", 1, 10, "B"]
+
+// console.log(newArr.indexOf("B"))
+// newArr.forEach(each => console.log(each))
