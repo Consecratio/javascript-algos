f10bb8152deb303b7d6b3f8ea241597fb63a4a48
Consecratio <jcasique@gmail.com>
2021-08-31 09:53:45 -0700

    added find_nth_prime description

--- find_nth_prime.js
+++ find_nth_prime.js
@@ -1,16 +1,24 @@
 /*
 SIMILAR
 -------------------------------------------------------------------------
-
+https://leetcode.com/discuss/general-discussion/573063/how-to-efficiently-find-nth-prime-number
 
 PROMPT
 -------------------------------------------------------------------------
-Write a function that takes an integer and finds the nth prime number
+Write a function that takes an int (n) and returns the n'th prime number in the sequence.
+
+For example:
+n = 5
+return 11
+
+
+Definition of Prime Number:
+A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers.
 
 */
 
 const findNthPrime = () => {
-    return 0
+	return 0
 }
 
 console.log(findNthPrime(5))
