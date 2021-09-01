b6ee49ebe4a2d378ac6679b5275cbbc45539a196
Consecratio <jcasique@gmail.com>
2021-09-01 09:19:01 -0700

    added fibonacci algo

---
+++ fibonacci.js
@@ -0,0 +1,30 @@
+/*
+SIMILAR
+-------------------------------------------------------------------------
+
+PROMPT
+-------------------------------------------------------------------------
+Write a function, that takes an integer (n), and returns
+the n'th number in the fibonacci sequence.
+
+For example, fib(5) would return 5 as 5 is the fifth number
+in the fibonacci sequence.
+
+The fibonacci sequence works as follows:
+    - Each number in the sequence is the sum of the two numbers that precede it.
+    - First 10 numbers of the sequence (starting with 1):
+    [1, 1, 2, 3, 5, 6, 13, 21, 34, 55]
+
+Params:
+1 <= n <= 100
+
+*/
+
+const fib = (n) => {
+
+}
+
+console.log(fib(3))
+console.log(fib(4))
+console.log(fib(8))
+console.log(fib(100))
