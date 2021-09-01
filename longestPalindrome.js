b999a0329d68760421903204c9c53c5e3e136484
Consecratio <jcasique@gmail.com>
2021-09-01 10:18:45 -0700

    added longest palindrome

---
+++ longestPalindrome.js
@@ -0,0 +1,20 @@
+/*
+
+SIMILAR
+-------------------------------------------------------------------------
+https://leetcode.com/problems/longest-palindromic-substring/
+
+PROMPT
+-------------------------------------------------------------------------
+Given a string s, return the longest palindromic substring in s.
+
+*/
+
+const longestPalindrome = (s) => {
+    return ""
+}
+
+console.log(longestPalindrome("babad")) // Output: "bab"
+console.log(longestPalindrome("cbbd")) // Output: "bb"
+console.log(longestPalindrome("a")) // Output: "a"
+console.log(longestPalindrome("ac")) // Output: "a"
