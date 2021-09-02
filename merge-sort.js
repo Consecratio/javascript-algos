062f79a408b4d966682b8d3f683fc9e3e87f802a
Consecratio <jcasique@gmail.com>
2021-09-02 10:28:14 -0700

    add merge-sort algo

---
+++ merge-sort.js
@@ -0,0 +1,17 @@
+/*
+SIMILAR
+-------------------------------------------------------------------------
+https://leetcode.com/problems/merge-sorted-array/
+
+PROMPT
+-------------------------------------------------------------------------
+Write a Merge Sort algo to sort the array [3, 4, 5, 1, 2, 6]
+*/
+
+const mergeSort = (arr) => {
+    return []
+}
+
+// WRITE YOUR CODE ABOVE THIS LINE
+
+console.log(mergeSort([3, 4, 5, 1, 2, 6])) // returns [1, 2, 3, 4, 5, 6]
