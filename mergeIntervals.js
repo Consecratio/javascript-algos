4b691ae37440e492b9263268743e32686709f838
Consecratio <jcasique@gmail.com>
2021-09-02 10:35:09 -0700

    add mergeIntervals.js

---
+++ mergeIntervals.js
@@ -0,0 +1,29 @@
+/*
+SIMILAR
+-------------------------------------------------------------------------
+https://leetcode.com/problems/meeting-rooms/
+https://leetcode.com/problems/meeting-rooms-ii/
+https://leetcode.com/problems/merge-intervals/
+
+PROMPT
+-------------------------------------------------------------------------
+Given two arrays (arrival times, duration times) of meetings, write a function
+to return the max number of meetings that can be held without overlapping.
+
+Find number of max meetings that can be held.
+arrival times = [978, 409, 229, 934, 299, 982, 636, 14, 866, 815, 64, 537, 426, 670, 116, 95, 630]
+duration times = [502, 518, 196, 106, 405, 452, 299, 189, 124, 506, 883, 753, 567, 717, 338, 439, 145]
+This test should return 4
+*/
+
+const meetings = (arrivals, durations) => {
+    return 0
+}
+
+// CODE ABOVE THIS LINE
+
+let arrivalTimes = [978, 409, 229, 934, 299, 982, 636, 14, 866, 815, 64, 537, 426, 670, 116, 95, 630] 
+
+let durationTimes = [502, 518, 196, 106, 405, 452, 299, 189, 124, 506, 883, 753, 567, 717, 338, 439, 145]
+
+console.log(metings(arrivalTimes, durationTimes)) // returns 4
