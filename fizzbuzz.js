b9b12e1ef7f2d028dde4693aca23dab2508fb20f
Consecratio <jcasique@gmail.com>
2021-09-07 20:09:58 -0700

    added fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+/*
+SIMILAR
+-------------------------------------------------------------------------
+
+PROMPT
+-------------------------------------------------------------------------
+Write a function that prints out each integer from 1 to 100, and if the
+number is evenly divisible by 3 print out Fizz, and if it is evenly
+divisible by 5 print out Buzz, and if it is divisible by both 3 and 5
+print out FizzBuzz.
+
+*/
+
+const fizzbuzz = () => {
+
+}
+
+// CODE ABOVE THIS LINE
+
+fizzbuzz()
